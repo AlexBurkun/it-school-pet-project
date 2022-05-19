1c1b1450b949d45807d8cc51fcfce1e35db71a40
Alexander <boorkoon@gmail.com>
2022-05-19 21:09:36 +0300

    Students table with search options

---
+++ force-app/main/default/lwc/studentsAssignment/studentsAssignment.js
@@ -0,0 +1,30 @@
+import { LightningElement, api, wire } from 'lwc';
+import getStudents from '@salesforce/apex/StudentsAssignmentController.getStudents';
+
+const DELAY = 300;
+
+export default class studentsAssignment extends LightningElement {
+
+    searchKey = '';
+    @api recordId;
+    students = [];
+
+    @wire(getStudents, { currentCourse: '$recordId', searchKey: '$searchKey' })
+    wiredStudents({ error, data }) {
+        if (data) {
+            this.students = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.data = undefined;
+        }
+    }
+
+    handleKeyChange(event) {
+        window.clearTimeout(this.delayTimeout);
+        const searchKey = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.searchKey = searchKey;
+        }, DELAY);
+    }
+}
