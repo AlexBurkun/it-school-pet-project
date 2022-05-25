d579fc3f3d00407ea13776cb50eac5d8df278ca1
Alexander <boorkoon@gmail.com>
2022-05-25 16:07:16 +0300

    Add dual-listbox visual list

--- force-app/main/default/lwc/manageStudentsModal/manageStudentsModal.js
+++ force-app/main/default/lwc/manageStudentsModal/manageStudentsModal.js
@@ -1,9 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import getAllStudents from '@salesforce/apex/StudentsAssignmentController.getAllStudents';
 
 export default class ManageStudentsModal extends LightningElement {
 
+    @api recordId;
+    @api registeredStudents = [];
+    @track options = [];
+    @track values = [];
+    selectedStudents = [];
+
     closeModal() {
         this.dispatchEvent(new CustomEvent("closemodal"));
+    }
+
+    connectedCallback() {
+        getAllStudents()
+        .then(data => {
+            this.options = data.map(record => ({ label: record.name, value: record.id }));
+            this.values = this.registeredStudents.map(record => record.id);
+        })
+        .catch(error => {
+            this.error = error;
+        })
+    }
+
+    handleChange(event) {
+        this.selectedStudents = event.detail.value;
     }
     
 }
